c63fd107a25c3753ca6d8b898159db9e0f199fe7
Vidar Farvald <vidar.farvald@gmail.com>
2024-01-07 12:30:02 +0100

    Fix Vitest types not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
-    global: true,
+    globals: true,
     environment: 'happy-dom',
   },
   optimizeDeps: {
